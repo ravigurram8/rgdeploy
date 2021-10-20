ee1d15143772727ce61ba2ac6291bb16ba864cf0
Puneet <puneet@relevancelab.com>
2021-10-20 14:29:07 +0530

    Added the config templates needed to create the AMI

---
+++ config/app-config.js
@@ -0,0 +1,25 @@
+var mkdirp = require('mkdirp');
+var fs = require('fs');
+var currentDirectory = __dirname;
+var logger = require('../logger/logger')(module);
+
+var configJson;
+try {
+	    configJson = fs.readFileSync(currentDirectory + '/dashboard-settings.json', {
+		            'encoding': 'utf8'
+		        });
+} catch (err) {
+	    logger.error(err);
+	    configJson = null;
+	    throw err;
+}
+
+if (configJson) {
+	    var config = JSON.parse(configJson);
+}
+
+mkdirp.sync(config.tempDashboardHome);
+mkdirp.sync(config.dashboardHome);
+
+module.exports = config;
+
