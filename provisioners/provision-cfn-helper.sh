bf0b2a54a366aaaca4f267c04c942d99620f8a34
deepashre <86648444+deepashre@users.noreply.github.com>
2021-10-22 11:49:56 +0530

    Update provision-cfn-helper.sh

--- provisioners/provision-cfn-helper.sh
+++ provisioners/provision-cfn-helper.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -xe
-apt-get update
-apt-get -y install python-pip
-pip install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
-cp /usr/local/init/ubuntu/cfn-hup /etc/init.d/cfn-hup
-chmod +x /etc/init.d/cfn-hup
-update-rc.d cfn-hup defaults
-service cfn-hup start 
+sudo apt-get update -y
+sudo apt -y install python3-pip
+sudo mkdir -p /opt/aws/bin
+sudo wget https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-py3-latest.tar.gz
+sudo python3 -m easy_install --script-dir /opt/aws/bin aws-cfn-bootstrap-py3-latest.tar.gz
+sudo cp /opt/aws/bin/cfn-hup /etc/init.d/cfn-hup
+sudo chmod +x /etc/init.d/cfn-hup
+sudo update-rc.d cfn-hup defaults
