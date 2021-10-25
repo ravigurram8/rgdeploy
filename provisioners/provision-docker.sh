7f89809075eb7fbb33d1d8f53a55a5280804f7e1
deepashre <86648444+deepashre@users.noreply.github.com>
2021-10-25 14:22:54 +0530

    Update provision-docker.sh

--- provisioners/provision-docker.sh
+++ provisioners/provision-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get clean
-sudo apt-get update
+sudo apt-get update -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg |sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
