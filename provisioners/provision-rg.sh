0ec5d465137895c635cd0b227d159470d5897f23
deepashre <86648444+deepashre@users.noreply.github.com>
2021-10-25 16:59:32 +0530

    Update provision-rg.sh

--- provisioners/provision-rg.sh
+++ provisioners/provision-rg.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
-sudo mkdir -p "/opt/deploy/sp2"
-sudo mkdir -p "/opt/deploy/sp2/logs"
-sudo mkdir -p "/opt/deploy/sp2/slogs"
-sudo mkdir -p "/opt/deploy/sp2/notification_sink_logs"
-sudo mkdir -p "/opt/deploy/sp2/configs"
-sudo mkdir -p "/opt/deploy/sp2/integrations"
-sudo mkdir -p "/opt/deploy/sp2/topologies"
-sudo mkdir -p "/opt/deploy/sp2/catalog"
-sudo cp /home/ubuntu/nginx.conf /opt/deploy/sp2
-sudo echo -e "RG_HOME='/opt/deploy/sp2'\n" >> /etc/environment
-cd /home/ubuntu
+sudo mkdir -p "${RG_HOME}"
+sudo mkdir -p "${RG_HOME}/logs"
+sudo mkdir -p "${RG_HOME}/slogs"
+sudo mkdir -p "${RG_HOME}/notification_sink_logs"
+sudo mkdir -p "${RG_HOME}/config"
+sudo mkdir -p "${RG_HOME}/integrations"
+sudo mkdir -p "${RG_HOME}/topologies"
+sudo mkdir -p "${RG_HOME}/catalog"
+sudo cp ${RG_SRC}/nginx.conf  ${RG_HOME}
+sudo echo -e "RG_HOME='/opt/deploy/sp2'\n" >> sudo /etc/environment
+cd ${RG_SRC}
 sudo tar -czf config.tar.gz config
