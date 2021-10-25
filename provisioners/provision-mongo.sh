c44f1dd2f9421ccdc1c1e24490377c74113338c6
deepashre <86648444+deepashre@users.noreply.github.com>
2021-10-25 11:53:17 +0530

    Update provision-mongo.sh

--- provisioners/provision-mongo.sh
+++ provisioners/provision-mongo.sh
@@ -1,8 +1,9 @@
-apt-get update -y
-apt-get install gnupg -y
-wget -qO - https://www.mongodb.org/static/pgp/server-${MONGO_VERSION}.asc | apt-key add -
-touch /etc/apt/sources.list.d/mongodb-org-${MONGO_VERSION}.list
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/${MONGO_VERSION} multiverse" | tee /etc/apt/sources.list.d/mongodb-org-${MONGO_VERSION}.list
-apt-get update -y
-apt-get install -y mongodb-org
-cp /tmp/mongod.conf /etc/mongod.conf
+#!/bin/bash
+sudo wget -qO - https://www.mongodb.org/static/pgp/server-${MONGO_VERSION}.asc | sudo apt-key add -
+sudo echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/${MONGO_VERSION} multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-${MONGO_VERSION}.list
+sudo apt-get update -y
+sudo apt-get install -y mongodb-org
+sudo apt-get install -y mongodb-org=${MONGO_VERSION}.${MONGO_Full_VERSION} mongodb-org-server=${MONGO_VERSION}.${MONGO_Full_VERSION}  mongodb-org-shell=${MONGO_VERSION}.${MONGO_Full_VERSION}  mongodb-org-mongos=${MONGO_VERSION}.${MONGO_Full_VERSION}  mongodb-org-tools=${MONGO_VERSION}.${MONGO_Full_VERSION}
+sudo systemctl daemon-reload
+sudo systemctl start mongod
+sudo systemctl stop mongod
