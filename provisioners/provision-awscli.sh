780e859281fe7d0717d7b4b63cc96be4b8bb53d4
deepashre <86648444+deepashre@users.noreply.github.com>
2021-10-26 11:30:33 +0530

    Update provision-awscli.sh

--- provisioners/provision-awscli.sh
+++ provisioners/provision-awscli.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -xe
+sudo apt-get install -y jq
+sudo apt-get update -y
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt install unzip
 sudo unzip awscliv2.zip
