56bf1a5266f6586f467a28e4f3f19aaed4d04df3
Sundeep <puneet@relevancelab.com>
2022-01-05 11:21:26 +0530

    Linting fixes

--- deploy.sh
+++ deploy.sh
@@ -23,23 +23,23 @@ if [ "$1" = "-f" ]; then
     exit 1
   fi
   echo "Reading past run details from $2"
-  myinput=`cat "$2"`
-  amiid=$(jq -r '.params.amiid' <<< ${myinput})
-  bucketname=$(jq -r '.params.bucketname' <<< ${myinput})
-  vpcid=$(jq -r '.params.vpcid' <<< ${myinput})
-  subnet1id=$(jq -r '.params.subnet1id' <<< ${myinput})
-  subnet2id=$(jq -r '.params.subnet2id' <<< ${myinput})
-  subnet3id=$(jq -r '.params.subnet3id' <<< ${myinput})
-  keypairname=$(jq -r '.params.keypairname' <<< ${myinput})
-  env=$(jq -r '.params.environment' <<< ${myinput})
-  rgurl=$(jq -r '.params.rgurl' <<< ${myinput})
-  tgarn=$(jq -r '.params.tgarn' <<< ${myinput})
+  myinput=$(cat "$2")
+  amiid=$(jq -r '.params.amiid' <<< "${myinput}")
+  bucketname=$(jq -r '.params.bucketname' <<< "${myinput}")
+  vpcid=$(jq -r '.params.vpcid' <<< "${myinput}")
+  subnet1id=$(jq -r '.params.subnet1id' <<< "${myinput}")
+  subnet2id=$(jq -r '.params.subnet2id' <<< "${myinput}")
+  subnet3id=$(jq -r '.params.subnet3id' <<< "${myinput}")
+  keypairname=$(jq -r '.params.keypairname' <<< "${myinput}")
+  env=$(jq -r '.params.environment' <<< "${myinput}")
+  rgurl=$(jq -r '.params.rgurl' <<< "${myinput}")
+  tgarn=$(jq -r '.params.tgarn' <<< "${myinput}")
 
-  runid=$(jq -r '.runid' <<< ${myinput})
-  appuser=$(jq -r '.appuser' <<< ${myinput})
-  appuserpassword=$(jq -r '.appuserpassword' <<< ${myinput})
-  adminpassword=$(jq -r '.adminpassword' <<< ${myinput})
-  S3_SOURCE=$(jq -r '.s3src' <<< ${myinput})
+  runid=$(jq -r '.runid' <<< "${myinput}")
+  appuser=$(jq -r '.appuser' <<< "${myinput}")
+  appuserpassword=$(jq -r '.appuserpassword' <<< "${myinput}")
+  adminpassword=$(jq -r '.adminpassword' <<< "${myinput}")
+  S3_SOURCE=$(jq -r '.s3src' <<< "${myinput}")
   echo "Run ID: $runid"
   echo "APPUSER: $appuser"
   echo "APPUSERPWD: $appuserpassword"
@@ -111,55 +111,55 @@ else
   }
 EOT
 fi
-aws ec2 describe-images --image-id $amiid >/dev/null 2>&1
+aws ec2 describe-images --image-id "$amiid" >/dev/null 2>&1
 if [ $? -gt 0 ]; then
    echo "The AMI $amiid does not exist. Exiting"
    exit 1
 fi
 
-if ! [ `echo $bucketname | grep -P '(?=^.{3,63}$)(?!^xn\-\-)(?!.*s3alias$)(?!^(\d+\.)+\d+$)(^(([a-z0-9]|[a-z0-9][a-z0-9\-]*[a-z0-9])\.)*([a-z0-9]|[a-z0-9][a-z0-9\-]*[a-z0-9])$)'` ]; then
+if ! [ $(echo "$bucketname" | grep -P '(?=^.{3,63}$)(?!^xn\-\-)(?!.*s3alias$)(?!^(\d+\.)+\d+$)(^(([a-z0-9]|[a-z0-9][a-z0-9\-]*[a-z0-9])\.)*([a-z0-9]|[a-z0-9][a-z0-9\-]*[a-z0-9])$)') ]; then
   echo "Invalid bucketname passed"
   echo "See https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html"
   exit 1
 fi
 
-aws ec2 describe-vpcs --vpc-id $vpcid >/dev/null 2>&1
+aws ec2 describe-vpcs --vpc-id "$vpcid" >/dev/null 2>&1
 if [ $? -gt 0 ]; then
    echo "The VPC $vpcid does not exist. Exiting."
    exit 1
 fi
 
-aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpcid --subnet-ids $subnet1id >/dev/null 2>&1
+aws ec2 describe-subnets --filters Name=vpc-id,Values="$vpcid" --subnet-ids "$subnet1id" >/dev/null 2>&1
 if [ $? -gt 0 ]; then
    echo "The subnet $subnet1id does not belong to $vpcid. Exiting."
    exit 1
 fi
 
-aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpcid --subnet-ids $subnet2id >/dev/null 2>&1
+aws ec2 describe-subnets --filters Name=vpc-id,Values="$vpcid" --subnet-ids "$subnet2id" >/dev/null 2>&1
 if [ $? -gt 0 ]; then
    echo "The subnet $subnet2id does not belong to $vpcid. Exiting."
    exit 1
 fi
 
-aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpcid --subnet-ids $subnet3id >/dev/null 2>&1
+aws ec2 describe-subnets --filters Name=vpc-id,Values="$vpcid" --subnet-ids "$subnet3id" >/dev/null 2>&1
 if [ $? -gt 0 ]; then
    echo "The subnet $subnet3id does not belong to $vpcid. Exiting."
    exit 1
 fi
 
-aws ec2 describe-key-pairs --key-name $keypairname >/dev/null 2>&1
+aws ec2 describe-key-pairs --key-name "$keypairname" >/dev/null 2>&1
 if [ $? -gt 0 ]; then
    echo "The KeyPair provided is not found. Exiting."
    exit 1
 fi
 
-echo $rgurl | grep -i -e '^http'
+echo "$rgurl" | grep -i -e '^http'
 if [ $? -gt 0 ]; then
    echo "The URL $rgurl must begin with http. Exiting."
    exit 1
 fi
 
-aws elbv2 describe-target-groups --target-group-arns $tgarn > /dev/null
+aws elbv2 describe-target-groups --target-group-arns "$tgarn" > /dev/null
 if [ $? -gt 0 ]; then
    echo "The Target Group ARN $tgarn does not exists. Exiting."
    exit 1
@@ -172,14 +172,14 @@ function calculate_duration() {
    eval "echo $mylabel Elapsed time: $(date -ud "@$myelapsedtime" +'$((%s/3600/24)) %M min %S sec')"
 }
 
-localhome=`pwd`
+localhome=$(pwd)
 bucketstackname="RG-PortalStack-Bucket-$runid"
 start_time=$SECONDS
 
 echo "Update Parameter Store"
 source scripts/updatessmpaths.sh
 
-BUCKET_TEST=`aws s3api head-bucket --bucket $bucketname 2>&1`
+BUCKET_TEST=$(aws s3api head-bucket --bucket "$bucketname" 2>&1)
 if [ -z "$BUCKET_TEST" ]; then
   echo "Bucket $bucketname exists, Hit Enter to continue, Ctrl-C to exit"
   read a && echo "Copying files to bucket $bucketname"
@@ -193,7 +193,7 @@ else
   aws cloudformation deploy --template-file rg_deploy_bucket.yml --stack-name "$bucketstackname" \
                             --parameter-overrides S3NewBucketName="$bucketname"
   echo "Waiting for stack $bucketname to finish deploying..."
-  aws cloudformation wait stack-create-complete --stack-name $bucketstackname
+  aws cloudformation wait stack-create-complete --stack-name "$bucketstackname"
   if [ $? -eq 255 ]; then
     echo "Failed to deploy stack $bucketstackname Exiting"
     exit 1
@@ -202,7 +202,7 @@ fi
 # Populate the new S3 bucket with RG Deployment files default source bucketname rg-newdeployment-docs
 echo "Synching RG Deployment Files to new S3 bucket $bucketname"
 s3_sync_start_time=$SECONDS
-aws s3 sync s3://$S3_SOURCE s3://$bucketname
+aws s3 sync s3://$S3_SOURCE s3://"$bucketname"
 calculate_duration "S3 Sync" $s3_sync_start_time
 
 #Create local folder to store RG Deployment Files and a subfolder for cft templates and scripts
@@ -211,34 +211,34 @@ mkdir -p "$localhome/rg-cft-templates"
 #Download RG Deployment files from S3 to the local folder created above,
 echo "Copying RG Deployment Files to local folder"
 s3_copy_start_time=$SECONDS
-aws s3 cp s3://$S3_SOURCE/ $localhome/rg-deployment-docs --recursive
+aws s3 cp s3://$S3_SOURCE/ "$localhome"/rg-deployment-docs --recursive
 calculate_duration "S3 Copy" $s3_copy_start_time
 
 # Extract cft templates locally
 echo "Extracting CFTs locally"
-tar -xvf $localhome/rg-deployment-docs/rg-cft-templates.tar.gz -C $localhome/rg-cft-templates/
+tar -xvf "$localhome"/rg-deployment-docs/rg-cft-templates.tar.gz -C "$localhome"/rg-cft-templates/
 
 #Modify file rg_userpool.yml to refer new S3 bucket
-sed -i -e "s/S3Bucket:.*/S3Bucket: $bucketname/" $localhome/rg_userpool.yml
+sed -i -e "s/S3Bucket:.*/S3Bucket: $bucketname/" "$localhome"/rg_userpool.yml
 
 #Copy extracted cft template to the new bucket
 echo "Copying deployment files to new bucket"
-aws s3 sync $localhome/rg-cft-templates/ s3://$bucketname
+aws s3 sync "$localhome"/rg-cft-templates/ s3://"$bucketname"
 #=====================================================================================================
 function get_stack_status() {
     stackname=$1
     stack_status=0
     jqcmd='.StackSummaries | map(select(.StackName=='"\"$stackname\""')|  .u=(.LastUpdatedTime[:16] | strptime("%Y-%m-%dT%H:%M") | mktime)) | sort_by(.u)| .[-1]'
     #echo $jqcmd
-    stack_exists=`echo $stack_summaries | jq -r "$jqcmd"`
+    stack_exists=$(echo "$stack_summaries" | jq -r "$jqcmd")
 
     if [ -z "$stack_exists" ]; then
     # stack does not exist    
 	    return 0
     fi
     #echo $stack_exists | jq -r
-    stack_status=`echo $stack_exists | jq -r '.StackStatus'`
-    echo $stack_status
+    stack_status=$(echo "$stack_exists" | jq -r '.StackStatus')
+    echo "$stack_status"
     #if [ "$stack_status" == "CREATE_COMPLETE" ]; then
     if [ "$stack_status" == "CREATE_COMPLETE" ] || [ "$stack_status" == "UPDATE_COMPLETE" ] || [ "$stack_status" == "UPDATE_COMPLETE_CLEANUP_IN_PROGRESS" ] || [ "$stack_status" == "UPDATE_ROLLBACK_COMPLETE" ]; then
          return 1
@@ -255,12 +255,12 @@ function get_stack_status() {
 
 function delete_stack() {
     echo "Deleting stack $1"
-    aws cloudformation delete-stack --stack-name $1
+    aws cloudformation delete-stack --stack-name "$1"
 }
 
 function create_cognito_pool() {
     echo "Creating new stack $1"
-    aws cloudformation deploy --template-file $localhome/rg_userpool.yml \
+    aws cloudformation deploy --template-file "$localhome"/rg_userpool.yml \
                           --stack-name "$1" \
                           --parameter-overrides UserPoolNameParam="$1" PortalURLParam="$rgurl" \
                             Function1Name="UserManagementAfterSuccessSignup-$runid" Function2Name="UserManagement-$runid" \
@@ -272,29 +272,29 @@ function create_cognito_pool() {
 function create_main_stack() {
     echo "Creating new stack $1"
     #update the AMI id in the RGMainStack CFT
-    echo $amiid | grep -E '^ami-[0-9a-zA-Z]+'
+    echo "$amiid" | grep -E '^ami-[0-9a-zA-Z]+'
     if [ $? -eq 0 ]; then
        echo "Valid AMI Id $amiid passed. Replacing in RGMainStack"
-       sed -i -E "s/ami-[0-9a-zA-Z]+/$amiid/" $localhome/rg_main_stack.yml
+       sed -i -E "s/ami-[0-9a-zA-Z]+/$amiid/" "$localhome"/rg_main_stack.yml
     fi
     echo "UserPool Id: $userpool_id"
     echo "UserPool ClientId: $userpoolclient_id"
     echo "BucketName $bucketname"
     echo "RG URL: $rgurl"
-    if [ ! -z $appuserpassword ]; then
+    if [ ! -z "$appuserpassword" ]; then
       echo "UserPassword is not a blank string"
     fi
-    if [ ! -z $adminpassword ]; then
+    if [ ! -z "$adminpassword" ]; then
       echo "AdminPassword is not a blank string"
     fi
     echo "VPC Id: $vpcid"
     echo "subnet1id: subnet1id"
     echo "Key Pair: $keypairname"
-    echo "TGARN: $rgarn"
+    echo "TGARN: $tgarn"
     echo "DocDBURL: $docdburl"
     echo "Env: $env"
     echo "Source Bucket: $S3_SOURCE"
-    aws cloudformation deploy --template-file $localhome/rg_main_stack.yml \
+    aws cloudformation deploy --template-file "$localhome"/rg_main_stack.yml \
                           --stack-name "$mainstackname" \
                           --parameter-overrides ClientId="$userpoolclient_id" UserPoolId="$userpool_id" \
                             CFTBucketName="$bucketname" RGUrl="$rgurl" UserPassword="$appuserpassword" AdminPassword="$adminpassword" \
@@ -306,7 +306,7 @@ function create_main_stack() {
         aws cloudformation wait stack-create-complete --stack-name "$mainstackname"
 }
 #===============================================================================================================
-stack_summaries=`aws cloudformation list-stacks`
+stack_summaries=$(aws cloudformation list-stacks)
 #===============================================================================================================
 #Creating the Cognito User Pool
 echo "Creating Cognito User Pool"
@@ -323,7 +323,7 @@ if [ $stack_status -eq 3 ]; then
     exit 1
 fi
 if [ $stack_status -eq 2 ]; then
-    delete_stack $userpoolstackname
+    delete_stack "$userpoolstackname"
     if [ $? -gt 0 ]; then
        echo "Could not delete stack $userpoolstackname"
        exit 1
@@ -331,14 +331,14 @@ if [ $stack_status -eq 2 ]; then
     stack_status=0
 fi
 if [ $stack_status -eq 0 ]; then
-    create_cognito_pool $userpoolstackname
+    create_cognito_pool "$userpoolstackname"
 fi
 
 #Capture User Pool Client ID
 userpoolclient_id=$(aws cloudformation describe-stack-resources --stack-name "$userpoolstackname" --logical-resource-id CognitoUserPoolClient | jq -r '.StackResources [] | .PhysicalResourceId')
 #Capture User Pool ID
 userpool_id=$(aws cloudformation describe-stack-resources --stack-name "$userpoolstackname" --logical-resource-id CognitoUserPool | jq -r '.StackResources [] | .PhysicalResourceId')
-if [ -z $userpoolclient_id ] || [ -z $userpool_id ]; then
+if [ -z "$userpoolclient_id" ] || [ -z "$userpool_id" ]; then
   echo "Unable to extract user pool outputs. Exiting."
   exit 1
 fi
@@ -354,11 +354,12 @@ echo "$docdbstackname"
 eval "get_stack_status $docdbstackname"
 stack_status=$?
 echo $stack_status
+docdb_start_time=$SECONDS
 if [ $stack_status -eq 3 ]; then
     exit 1
 fi
 if [ $stack_status -eq 2 ]; then
-    delete_stack $docdbstackname
+    delete_stack "$docdbstackname"
     if [ $? -gt 0 ]; then
        echo "Could not delete stack $docdbstackname"
        exit 1
@@ -366,29 +367,29 @@ if [ $stack_status -eq 2 ]; then
     stack_status=0
 fi
 if [ $stack_status -eq 0 ]; then
-    create_doc_db $docdbstackname
+    create_doc_db "$docdbstackname"
 fi
-calculate_duration "DocumentDB Instance Creation" $docdb_start_time
+calculate_duration "DocumentDB Instance Creation" "$docdb_start_time"
 
 #Capture DocumentDB Instance Id
-docdburl=$(aws cloudformation describe-stacks --stack-name $docdbstackname | jq -r '.Stacks[] | .Outputs[] | select(.OutputKey=="InstanceEndpoint")|.OutputValue')
-if [ -z $docdburl ]; then
+docdburl=$(aws cloudformation describe-stacks --stack-name "$docdbstackname" | jq -r '.Stacks[] | .Outputs[] | select(.OutputKey=="InstanceEndpoint")|.OutputValue')
+if [ -z "$docdburl" ]; then
   echo "Unable to extract DocumentDB outputs. Exiting."
   exit 1
 fi
 #===============================================================================================================
 #Creating Main stack
 #update the AMI id in the RGMainStack CFT
-echo $amiid | grep -E '^ami-[0-9a-zA-Z]+'
+echo "$amiid" | grep -E '^ami-[0-9a-zA-Z]+'
 if [ $? -eq 0 ]; then
   echo "Valid AMI Id $amiid passed. Replacing in RGMainStack"
-  sed -i -E "s/ami-[0-9a-zA-Z]+/$amiid/" $localhome/rg_main_stack.yml
+  sed -i -E "s/ami-[0-9a-zA-Z]+/$amiid/" "$localhome"/rg_main_stack.yml
 fi
 
 echo "Deploying main stack (roles, ec2 instance etc.)"
 mainstack_start_time=$SECONDS
 mainstackname="RG-PortalStack-$runid"
-aws cloudformation deploy --template-file $localhome/rg_main_stack.yml \
+aws cloudformation deploy --template-file "$localhome"/rg_main_stack.yml \
                           --stack-name "$mainstackname" \
                           --parameter-overrides ClientId="$userpoolclient_id" UserPoolId="$userpool_id" \
                             CFTBucketName="$bucketname" RGUrl="$rgurl" UserPassword="$appuserpassword" AdminPassword="$adminpassword" \
