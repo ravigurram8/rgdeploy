b5e1c5a62e4c3a026ae1ad8be2a12eb3b1be6f47
Prashansa Shanbhag <prashansashanbhag@gmail.com>
2022-01-24 10:37:26 +0530

    test

--- mainonly.sh
+++ mainonly.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Verify that utilities we use in this script are installed on the machine
-#this is a test
 echo "Verifying utilities are installed"
 apps=(jq aws)
 for program in "${apps[@]}"; do
