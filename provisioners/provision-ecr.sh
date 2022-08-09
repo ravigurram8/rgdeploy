2541a388f60d6166daf92debada73751ca17a86d
ravigurram8 <ravikumar.gurram@relevancelab.com>
2022-08-09 10:39:30 +0530

    Updated with 1.12.0 builds

--- provisioners/provision-ecr.sh
+++ provisioners/provision-ecr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 sudo docker login -u AWS -p $(aws ecr get-login-password --region us-east-2) 045938549113.dkr.ecr.us-east-2.amazonaws.com
-sudo docker pull 045938549113.dkr.ecr.us-east-2.amazonaws.com/researchportal:_fd_1.11.5_b1192
+sudo docker pull 045938549113.dkr.ecr.us-east-2.amazonaws.com/researchportal:_fd_1.12.0_b1235
 sudo docker pull 045938549113.dkr.ecr.us-east-2.amazonaws.com/nginx:latest
-sudo docker pull 045938549113.dkr.ecr.us-east-2.amazonaws.com/notificationsink:1.11.4_b1
+sudo docker pull 045938549113.dkr.ecr.us-east-2.amazonaws.com/notificationsink:1.12.1_b1
