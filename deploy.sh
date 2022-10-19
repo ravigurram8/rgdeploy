f26a38ab4a2b5d66bfe53c899f9ee066d059b932
ravigurram8 <ravikumar.gurram@relevancelab.com>
2022-10-19 19:22:57 +0530

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -338,7 +338,7 @@ function create_doc_db() {
 	echo "Creating new stack $1"
 	aws cloudformation deploy --template-file "$localhome"/rg_document_db.yml --stack-name "$1" \
         --parameter-overrides DBClusterName="RGCluster-$runid" DBInstanceName="RGInstance-$runid" \       
-		 DBInstanceClass="db.t3.medium "Subnet1="$subnet1id" Subnet2="$subnet2id" Subnet3="$subnet3id" \
+		 DBInstanceClass="db.t3.medium" "Subnet1="$subnet1id" Subnet2="$subnet2id" Subnet3="$subnet3id" \
      VPC="$vpcid" SecurityGroupName="RGDB-SG-$runid" DocDBSubnetGroupName="RGDBSubnet-$runid" DocDBSecretName="app/config/RG-$runid"
 	echo "Waiting for stack $1 to finish deploying..."
 	aws cloudformation wait stack-create-complete --stack-name "$1"
